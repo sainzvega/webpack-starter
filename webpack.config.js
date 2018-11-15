4b16c68623289cf2fd93ac4e270550b7a5e95ab3
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-11-15 13:16:17 -0800

    Remove console log from config. Add progress plugin to base config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
-  console.log(mode);
   return {
     mode: mode,
     output: {
-        filename: "bundle.js"
-    }
-    plugins: [new HtmlWebpackPlugin()]
+      filename: "bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
   };
 };
